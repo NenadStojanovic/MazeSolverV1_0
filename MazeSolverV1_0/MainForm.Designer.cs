cd972a485e6f1cdc5580fa4d0bb640a8d4bedb2c
NenadStojanovic <nenad.stojanovic@gmx.com>
2017-10-02 03:43:33 +0200

    Legend je gotova

--- MazeSolverV1_0/MainForm.Designer.cs
+++ MazeSolverV1_0/MainForm.Designer.cs
@@ -40,7 +40,6 @@
             this.aStarRB = new System.Windows.Forms.RadioButton();
             this.bfsRB = new System.Windows.Forms.RadioButton();
             this.dfsRB = new System.Windows.Forms.RadioButton();
-            this.generateMazeBtn = new System.Windows.Forms.Button();
             this.clearBtn = new System.Windows.Forms.Button();
             this.newGridBtn = new System.Windows.Forms.Button();
             this.colsTB = new System.Windows.Forms.TextBox();
@@ -52,6 +51,7 @@
             this.timeElapsedLabel = new System.Windows.Forms.Label();
             this.outputMsgLabel = new System.Windows.Forms.Label();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.groupBox5 = new System.Windows.Forms.GroupBox();
             this.labelSourceLink = new System.Windows.Forms.Label();
             this.SourcePathBtn = new System.Windows.Forms.Button();
@@ -60,15 +60,36 @@
             this.fileSystemWatcherSource = new System.IO.FileSystemWatcher();
             this.KeyGB = new System.Windows.Forms.GroupBox();
             this.KeyLabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
             this.MazeGridGB.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.mazePictureBox)).BeginInit();
             this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.DelayTrackBar)).BeginInit();
             this.Algorithms.SuspendLayout();
             this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox5.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcherSource)).BeginInit();
             this.KeyGB.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
             this.SuspendLayout();
             // 
             // MazeGridGB
@@ -98,7 +119,6 @@
             this.groupBox2.Controls.Add(this.DelayTrackBar);
             this.groupBox2.Controls.Add(this.solveMazeBtn);
             this.groupBox2.Controls.Add(this.Algorithms);
-            this.groupBox2.Controls.Add(this.generateMazeBtn);
             this.groupBox2.Controls.Add(this.clearBtn);
             this.groupBox2.Controls.Add(this.newGridBtn);
             this.groupBox2.Controls.Add(this.colsTB);
@@ -157,7 +177,7 @@
             // dijkstraRB
             // 
             this.dijkstraRB.AutoSize = true;
-            this.dijkstraRB.Location = new System.Drawing.Point(7, 89);
+            this.dijkstraRB.Location = new System.Drawing.Point(7, 55);
             this.dijkstraRB.Name = "dijkstraRB";
             this.dijkstraRB.Size = new System.Drawing.Size(60, 17);
             this.dijkstraRB.TabIndex = 4;
@@ -179,7 +199,7 @@
             // aStarRB
             // 
             this.aStarRB.AutoSize = true;
-            this.aStarRB.Location = new System.Drawing.Point(6, 55);
+            this.aStarRB.Location = new System.Drawing.Point(7, 89);
             this.aStarRB.Name = "aStarRB";
             this.aStarRB.Size = new System.Drawing.Size(36, 17);
             this.aStarRB.TabIndex = 2;
@@ -210,20 +230,11 @@
             this.dfsRB.UseVisualStyleBackColor = true;
             this.dfsRB.CheckedChanged += new System.EventHandler(this.dfsRB_CheckedChanged);
             // 
-            // generateMazeBtn
-            // 
-            this.generateMazeBtn.Location = new System.Drawing.Point(10, 103);
-            this.generateMazeBtn.Name = "generateMazeBtn";
-            this.generateMazeBtn.Size = new System.Drawing.Size(156, 23);
-            this.generateMazeBtn.TabIndex = 6;
-            this.generateMazeBtn.Text = "Generate Maze";
-            this.generateMazeBtn.UseVisualStyleBackColor = true;
-            // 
             // clearBtn
             // 
-            this.clearBtn.Location = new System.Drawing.Point(91, 74);
+            this.clearBtn.Location = new System.Drawing.Point(10, 104);
             this.clearBtn.Name = "clearBtn";
-            this.clearBtn.Size = new System.Drawing.Size(75, 23);
+            this.clearBtn.Size = new System.Drawing.Size(176, 23);
             this.clearBtn.TabIndex = 5;
             this.clearBtn.Text = "Clear";
             this.clearBtn.UseVisualStyleBackColor = true;
@@ -233,7 +244,7 @@
             // 
             this.newGridBtn.Location = new System.Drawing.Point(10, 74);
             this.newGridBtn.Name = "newGridBtn";
-            this.newGridBtn.Size = new System.Drawing.Size(75, 23);
+            this.newGridBtn.Size = new System.Drawing.Size(176, 23);
             this.newGridBtn.TabIndex = 4;
             this.newGridBtn.Text = "New Grid";
             this.newGridBtn.UseVisualStyleBackColor = true;
@@ -278,7 +289,7 @@
             this.groupBox3.Controls.Add(this.outputMsgLabel);
             this.groupBox3.Location = new System.Drawing.Point(13, 369);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(350, 85);
+            this.groupBox3.Size = new System.Drawing.Size(350, 114);
             this.groupBox3.TabIndex = 12;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Output";
@@ -309,12 +320,36 @@
             // 
             // groupBox4
             // 
+            this.groupBox4.Controls.Add(this.label10);
+            this.groupBox4.Controls.Add(this.pictureBox7);
+            this.groupBox4.Controls.Add(this.label8);
+            this.groupBox4.Controls.Add(this.pictureBox5);
+            this.groupBox4.Controls.Add(this.label9);
+            this.groupBox4.Controls.Add(this.pictureBox6);
+            this.groupBox4.Controls.Add(this.label6);
+            this.groupBox4.Controls.Add(this.pictureBox3);
+            this.groupBox4.Controls.Add(this.label7);
+            this.groupBox4.Controls.Add(this.pictureBox4);
+            this.groupBox4.Controls.Add(this.label5);
+            this.groupBox4.Controls.Add(this.pictureBox2);
+            this.groupBox4.Controls.Add(this.label4);
+            this.groupBox4.Controls.Add(this.pictureBox1);
             this.groupBox4.Location = new System.Drawing.Point(369, 369);
             this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(192, 85);
+            this.groupBox4.Size = new System.Drawing.Size(192, 114);
             this.groupBox4.TabIndex = 13;
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "Legend";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(7, 20);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(16, 16);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // groupBox5
             // 
@@ -324,7 +359,7 @@
             this.groupBox5.Controls.Add(this.lbSource);
             this.groupBox5.Location = new System.Drawing.Point(568, 13);
             this.groupBox5.Name = "groupBox5";
-            this.groupBox5.Size = new System.Drawing.Size(206, 441);
+            this.groupBox5.Size = new System.Drawing.Size(206, 470);
             this.groupBox5.TabIndex = 14;
             this.groupBox5.TabStop = false;
             this.groupBox5.Text = "Files Preview";
@@ -362,7 +397,7 @@
             this.lbSource.FormattingEnabled = true;
             this.lbSource.Location = new System.Drawing.Point(6, 78);
             this.lbSource.Name = "lbSource";
-            this.lbSource.Size = new System.Drawing.Size(194, 355);
+            this.lbSource.Size = new System.Drawing.Size(194, 381);
             this.lbSource.TabIndex = 1;
             this.lbSource.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lbSource_MouseDoubleClick);
             // 
@@ -376,9 +411,9 @@
             // KeyGB
             // 
             this.KeyGB.Controls.Add(this.KeyLabel);
-            this.KeyGB.Location = new System.Drawing.Point(13, 461);
+            this.KeyGB.Location = new System.Drawing.Point(8, 489);
             this.KeyGB.Name = "KeyGB";
-            this.KeyGB.Size = new System.Drawing.Size(760, 61);
+            this.KeyGB.Size = new System.Drawing.Size(766, 61);
             this.KeyGB.TabIndex = 15;
             this.KeyGB.TabStop = false;
             this.KeyGB.Text = "Key";
@@ -392,11 +427,134 @@
             this.KeyLabel.Size = new System.Drawing.Size(0, 20);
             this.KeyLabel.TabIndex = 0;
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Empty";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(105, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Obsticle";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Black;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(83, 20);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(16, 16);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(105, 45);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Target";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Green;
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.Location = new System.Drawing.Point(83, 42);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(16, 16);
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(29, 45);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 13);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Start";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Red;
+            this.pictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox4.Location = new System.Drawing.Point(7, 42);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(16, 16);
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(105, 67);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(42, 13);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Frontier";
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.Blue;
+            this.pictureBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox5.Location = new System.Drawing.Point(83, 64);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(16, 16);
+            this.pictureBox5.TabIndex = 10;
+            this.pictureBox5.TabStop = false;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(29, 67);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(39, 13);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "Closed";
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.Cyan;
+            this.pictureBox6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox6.Location = new System.Drawing.Point(7, 64);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(16, 16);
+            this.pictureBox6.TabIndex = 8;
+            this.pictureBox6.TabStop = false;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(29, 89);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(29, 13);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "Path";
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.Color.Yellow;
+            this.pictureBox7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox7.Location = new System.Drawing.Point(7, 86);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(16, 16);
+            this.pictureBox7.TabIndex = 12;
+            this.pictureBox7.TabStop = false;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(785, 534);
+            this.ClientSize = new System.Drawing.Size(784, 558);
             this.Controls.Add(this.KeyGB);
             this.Controls.Add(this.groupBox5);
             this.Controls.Add(this.groupBox4);
@@ -414,11 +572,20 @@
             this.Algorithms.PerformLayout();
             this.groupBox3.ResumeLayout(false);
             this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.groupBox5.ResumeLayout(false);
             this.groupBox5.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcherSource)).EndInit();
             this.KeyGB.ResumeLayout(false);
             this.KeyGB.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -436,7 +603,6 @@
         private System.Windows.Forms.RadioButton aStarRB;
         private System.Windows.Forms.RadioButton bfsRB;
         private System.Windows.Forms.RadioButton dfsRB;
-        private System.Windows.Forms.Button generateMazeBtn;
         private System.Windows.Forms.Button clearBtn;
         private System.Windows.Forms.Button newGridBtn;
         private System.Windows.Forms.TextBox colsTB;
@@ -457,6 +623,20 @@
         private System.Windows.Forms.Label KoordinatesLabel;
         private System.Windows.Forms.GroupBox KeyGB;
         private System.Windows.Forms.Label KeyLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label4;
     }
 }
 
